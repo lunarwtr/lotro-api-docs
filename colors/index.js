4d2b07fdedbca299bdd7018c007d922da5e4698b
Lunarwtr <lunarwtr@gmail.com>
2022-03-20 13:04:40 -0600

    - add index page to view all colors

--- colors/index.js
+++ colors/index.js
@@ -2,8 +2,9 @@ const { createCanvas } = require('canvas')
 const fs = require('fs');
 const colors = require('./colors.json');
 
-Object.entries(colors).forEach((e, i) => {
-    const [color, c] = e;
+const rows = [];
+Object.keys(colors).sort().forEach((color, i) => {
+    const c = colors[color];
     const can = createCanvas(20, 20);
     var ctx = can.getContext('2d');
     const style = `rgba(${Math.ceil(255*c.R.value)},${Math.ceil(255*c.G.value)},${Math.ceil(255*c.B.value)},${c.A.value})`;
@@ -12,10 +13,22 @@ Object.entries(colors).forEach((e, i) => {
     ctx.fillStyle = style;
     ctx.arc(10, 10, 10, 0, Math.PI * 2, false);
     ctx.fill();
-    console.log(`${color}(${style})`)
+    rows.push(`<tr><td style="color: ${style};">${color}</td><td><img src="${color}.png"/></td></tr>`)
     
     const out = fs.createWriteStream(__dirname + `/images/${color}.png`)
     const stream = can.createPNGStream()
     stream.pipe(out)
     out.on('finish', () =>  console.log(`The ${color}(${style}) PNG file was created.`))
+});
+
+var stream = fs.createWriteStream('images/index.html');
+stream.once('open', function(fd) {
+  var html = `<html>
+    <body>
+    <table>
+        ${ rows.join('\n') }
+    </table>
+    </body>
+  </html>`;
+  stream.end(html);
 });
